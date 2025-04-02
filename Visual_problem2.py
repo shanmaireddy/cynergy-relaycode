ca00172f6669e070a3ca8548fa93b1888b031fea
shanmaireddy <shanmaireddy.2006@gmail.com>
2025-04-02 01:05:57 -0700

    shanmai

---
+++ Visual_problem2.py
@@ -0,0 +1,32 @@
+#shanmai
+def r(a):
+    if a=="i":
+        return a==1
+    elif a=="ii" :
+        return a==2
+    elif a == "iii" :
+        return a == 3
+    elif a == "iv" :
+        return a == 4
+    elif a=="v" :
+        return a==5
+    elif a=="vi" :
+        return a==6
+    elif a=="vii" :
+        return a==7
+    elif a=="viii" :
+        return a==8
+    elif a=="ix" :
+        return a==9
+    elif a=="x" :
+        return a==10
+    elif a=="xi" :
+        return a==11
+w=input("Enter the 1st Number:    ")
+z=input("Enter the 2nd Number:   ")
+e=r(w)+r(z)
+print("The solution is ",e)
+
+
+
+
